bd459efe22c1066fe8d8ba23ef583fd795642788
Nour Elmaliki <nourelmaliki@gmail.com>
2024-12-19 23:16:32 -0500

    remove extra folder level

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  trailingSlash: true,
-  distDir: "../client"
+  trailingSlash: true
 };
 
 export default nextConfig;
